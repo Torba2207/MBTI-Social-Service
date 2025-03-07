a948f4f4b3d35f06622a98832c85042b9d2109ab
Dmytro Dzhusov <dmytro.dzhusov@gmail.com>
2025-03-07 19:59:34 +0100

    Implement Answer, AnswerSet, and Questions entities

---
+++ Backend/src/main/java/com/pg/mbti/entity/questions/Answer.java
@@ -0,0 +1,24 @@
+package com.pg.mbti.entity.questions;
+
+import com.pg.mbti.entity.MBTIType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "answer")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Answer {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
+    @ManyToOne
+    @JoinColumn(
+            name = "answer_set_id",
+            referencedColumnName = "id",
+            nullable = false
+    )
+    private AnswerSet answerSet;
+    @Column(name = "mbti") @Enumerated(EnumType.STRING) private MBTIType mbtiType;
+}
---
+++ Backend/src/main/java/com/pg/mbti/entity/questions/AnswerSet.java
@@ -0,0 +1,23 @@
+package com.pg.mbti.entity.questions;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "answer_set")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class AnswerSet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
+    @ManyToOne
+    @JoinColumn(
+            name = "question_id",
+            nullable = false,
+            referencedColumnName = "id"
+    )
+    private Questions question;
+}
