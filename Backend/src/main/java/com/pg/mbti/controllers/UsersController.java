c14be055d51c096198548a4e41532ee9aabb61c2
Dmytro Dzhusov <dmytro.dzhusov@gmail.com>
2025-03-09 13:11:46 +0100

    Add endpoint to retrieve user by nickname in UsersController

--- Backend/src/main/java/com/pg/mbti/controllers/UsersController.java
+++ Backend/src/main/java/com/pg/mbti/controllers/UsersController.java
@@ -4,10 +4,12 @@ import com.pg.mbti.entity.user.User;
 import com.pg.mbti.services.UsersService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
@@ -19,5 +21,10 @@ public class UsersController {
     @GetMapping("/users")
     public List<User> getUsers() {
         return usersService.getUsers();
+    }
+
+    @GetMapping("/users/{nickname}")
+    public User getUserByNickname(@PathVariable final String nickname) {
+        return usersService.getUserByNickname(nickname);
     }
 }
