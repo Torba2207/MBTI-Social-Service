f3492c28a5a0494b6f4e7cd3a29e1790e2765b1d
Torba2207 <alexnychyporchuk@gmail.com>
2025-03-14 22:30:19 +0100

    start: login page

---
+++ Frontend/m8bti-web-service/src/pages/loginPage.jsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import React, { useContext, useState} from "react";
+
+
+export default function Login(){
+    return(
+        <>
+            <div>
+                <Header/>
+            </div>
+        </>
+    )
+}
