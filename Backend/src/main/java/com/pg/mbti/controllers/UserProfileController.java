f53fbc5715a18353205b5008bfcf5d450b069a8c
Dmytro Dzhusov <dmytro.dzhusov@gmail.com>
2025-03-18 19:07:44 +0100

    feat: Add MinIO configuration and implement profile photo upload functionality

--- Backend/src/main/java/com/pg/mbti/controllers/UserProfileController.java
+++ Backend/src/main/java/com/pg/mbti/controllers/UserProfileController.java
@@ -2,29 +2,58 @@ package com.pg.mbti.controllers;
 
 import com.pg.mbti.dto.UserProfileDto;
 import com.pg.mbti.mappers.UserMapper;
+import com.pg.mbti.services.PhotoService;
 import com.pg.mbti.services.UsersService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import lombok.AllArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/api/user")
-@RequiredArgsConstructor
+@RequestMapping("/api/user/me")
+@AllArgsConstructor
 public class UserProfileController {
 
     private final UsersService usersService;
     private final UserMapper userMapper;
+    private PhotoService photoService;
 
-    @GetMapping("/me")
-    public ResponseEntity<UserProfileDto> getUserProfile(
-            final Authentication authentication) {
-
-        final var user =
-                usersService.getUserByNickname(authentication.getName());
-
+    @GetMapping
+    public ResponseEntity<UserProfileDto> getUserProfile(final Authentication authentication) {
+        final var user = usersService.getUserByNickname(authentication.getName());
         return ResponseEntity.ok(userMapper.toUserProfileDto(user));
+    }
+
+    @PostMapping("photo/upload")
+    public ResponseEntity<String> uploadProfilePhoto(Authentication authentication, @RequestParam("image") MultipartFile file) {
+        String fileName = photoService.uploadPhoto(file);
+        final var user = usersService.getUserByNickname(authentication.getName());
+        user.setProfilePicture(fileName);
+        usersService.updateUser(user);
+        return ResponseEntity.ok("File uploaded successfully: " + fileName);
+    }
+
+    @GetMapping("/photo")
+    public ResponseEntity<Resource> getProfilePhoto(Authentication authentication) {
+        try {
+            final var user = usersService.getUserByNickname(authentication.getName());
+            String fileName = user.getProfilePicture();
+            if (fileName == null) {
+                return ResponseEntity.notFound().build();
+            }
+
+            return ResponseEntity.ok()
+                    .contentType(MediaType.IMAGE_JPEG)
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                    .body(photoService.getPhoto(fileName));
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 }
