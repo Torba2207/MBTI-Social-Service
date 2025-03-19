3f7b53b6b5f04dd491793ba4839e66cf2c5e7275
Torba2207 <alexnychyporchuk@gmail.com>
2025-03-19 04:16:37 +0100

    perf: AuthLayout component now includes Logo component and handles children

--- Frontend/m8bti-web-service/src/components/AuthLayout.jsx
+++ Frontend/m8bti-web-service/src/components/AuthLayout.jsx
@@ -1,8 +1,17 @@
 import Link from 'next/link'
+import { Logo } from '@/components/Logo'
 
-export default function AuthLayout({title, subtitle, children}){
+export default function AuthLayout({children}){
     return(
-        <>
-        </>
+        <main>
+            <div className='flex w-full h-41/100 px-[20%]'>
+                <Link href="/" arial-label="Home" className='mx-auto'>
+                    <Logo className="h-full"/>
+                </Link>
+            </div>
+            <div className='mx-[30%] px-[5%] bg-[#D6ECE3]'>
+                {children}
+            </div>
+        </main>
     )
 }
