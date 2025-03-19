1ab13eb5e3bdad8a3be73f2a070162b582f68aed
Torba2207 <alexnychyporchuk@gmail.com>
2025-03-19 04:16:55 +0100

    add: TextFields in Login page

--- Frontend/m8bti-web-service/src/pages/loginPage.jsx
+++ Frontend/m8bti-web-service/src/pages/loginPage.jsx
@@ -1,14 +1,46 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import AuthLayout from '@/components/AuthLayout';
+import { TextField } from '@/components/Fields';
+import { Button } from '@/components/Button';
 import React, { useContext, useState} from "react";
 
 
 export default function Login(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [correctFields, setCorrectFields] = useState(true)
+
+
     return(
         <>
-            <div>
-                <Header/>
-            </div>
+            <Head>
+                <title>M8TI - Sign In</title>
+            </Head>
+            <AuthLayout>
+                <form>
+                    <div>
+                      <TextField
+                        label="Email or Profile Name"
+                        id="email"
+                        name="email"
+                        type="email"
+                        autoComplete="email"
+                        value={email}
+                        onChange={(e)=>{setEmail(e.target.value), setCorrectFields(true)}}
+                        required/>
+                        <TextField
+                        label="Password"
+                        id="password"
+                        name="password"
+                        type="password"
+                        autoComplete="current-password"
+                        value={password}
+                        onChange={(e)=>{setPassword(e.target.value), setCorrectFields(true)}}
+                        required/>
+                    </div>
+                </form>
+            </AuthLayout>
         </>
     )
 }
