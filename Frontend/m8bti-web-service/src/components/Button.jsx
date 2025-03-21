304af08136c9825754492c45db1e53d41613f05e
Torba2207 <alexnychyporchuk@gmail.com>
2025-03-21 03:48:15 +0100

    add: New styles for Button

--- Frontend/m8bti-web-service/src/components/Button.jsx
+++ Frontend/m8bti-web-service/src/components/Button.jsx
@@ -1,16 +1,18 @@
 import { forwardRef } from 'react'
 import Link from 'next/link'
 import clsx from 'clsx'
+import { MBTIColors } from './MBTIColors'
 
 const baseStyles = {
   solid:
-    'inline-flex justify-center rounded-lg py-2 px-3 text-sm font-semibold outline-2 outline-offset-2 transition-colors',
+    'inline-flex justify-center rounded-lg py-2 px-3 text-sm font-semibold transition-colors',
   outline:
     'inline-flex justify-center rounded-lg border py-[calc(theme(spacing.2)-1px)] px-[calc(theme(spacing.3)-1px)] text-sm outline-2 outline-offset-2 transition-colors',
 }
 
 const variantStyles = {
   solid: {
+    purple:'bg-[#785D87] text-[#F4EDF6] active:bg-[#482D57]', 
     cyan: 'relative overflow-hidden bg-cyan-500 text-white before:absolute before:inset-0 active:before:bg-transparent hover:before:bg-white/10 active:bg-cyan-600 active:text-white/80 before:transition-colors',
     white:
       'bg-white text-cyan-900 hover:bg-white/90 active:bg-white/90 active:text-cyan-900/70',
@@ -21,6 +23,7 @@ const variantStyles = {
     gray: 'border-gray-300 text-gray-700 hover:border-gray-400 active:bg-gray-100 active:text-gray-700/80',
   },
 }
+
 
 export const Button = forwardRef(function Button(
   { variant = 'solid', color = 'gray', className, href, ...props },
