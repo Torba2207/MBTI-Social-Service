1723184d47a911742e89c21eb7b1522f576644cb
Torba2207 <alexnychyporchuk@gmail.com>
2025-03-24 20:27:27 +0100

    Upd: Basic One-colored for log in user Header without nav panel

--- Frontend/m8bti-web-service/src/components/Header.jsx
+++ Frontend/m8bti-web-service/src/components/Header.jsx
@@ -7,16 +7,16 @@ export function Header({mbti}){
         <header className={"flex w-full h-1/5 border-b-2 border-gray-500"}
             style={{ backgroundColor: MBTIColors({ colorDest: "Secondary", mbti }) }}>
             {/**Logo Div */}
-            <div className=" flex w-15/100 h-2/2 flex items-center text-center ml-[5%]">
-                <Logo className="auto" />
+            <div className="flex w-1/3 min-w-[200px] h-1/2 min-h-[40px]">
+                <Logo className="w-auto h-auto" />
             </div>
             {/**Full Name Div */}
-            <div className="flex h-1/2 w-7/10 items-end">
-                <h1 className="w-9/10 text-3xl">Name SureName</h1>
+            <div className="flex h-1/2 w-7/10 align-top">
+                <h1 className="w-9/10 text-center text-2xl sm:text-3xl sm:text-left">Name SureName</h1>
             </div>
 
             {/**Avatar Div */}
-            <div className="w-1/10 h-1/2 flex items-end">
+            <div className="w-1/10 h-1/2 flex items-top">
                 <DefaultAvatar className="w-8 h-8 mb=2"/>
             </div>
 
