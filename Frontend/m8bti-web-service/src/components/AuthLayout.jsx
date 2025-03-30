bc4dd7decb05190a531b7fc8800c91c65360028c
Torba2207 <alexnychyporchuk@gmail.com>
2025-03-30 05:38:25 +0200

    feat: AuthLayout and loginPage in progress for dynamic recoloring

--- Frontend/m8bti-web-service/src/components/AuthLayout.jsx
+++ Frontend/m8bti-web-service/src/components/AuthLayout.jsx
@@ -4,13 +4,13 @@ import clsx from 'clsx'
 
 export default function AuthLayout({children, ...props}){
     return(
-        <main className='w-screen h-screen bg-[#E7DFEA] min-h-screen'>
+        <main className='w-screen h-screen min-h-screen'>
             <div className='w-screen h-[30%] mb-[1%]'>
                 <Link href="/" arial-label="Home">
                     <Logo className="w-auto h-auto mx-auto"/>
                 </Link>
             </div>
-            <div className={clsx(`w-[30%] min-w-[350px] mx-auto bg-[#F4EDF6] border-5 border-[#785D87] rounded-lg`, props.className)}>
+            <div className={clsx(`w-[30%] min-w-[350px] mx-auto border-5 rounded-lg`, props.className)} style={props.style}>
                 {children}
             </div>
         </main>
--- Frontend/m8bti-web-service/src/pages/loginPage.jsx
+++ Frontend/m8bti-web-service/src/pages/loginPage.jsx
@@ -5,15 +5,22 @@ import { TextField } from '@/components/Fields';
 import { Button } from '@/components/Button';
 import React, { useContext, useState} from "react";
 import clsx from 'clsx';
+import useColorCycle from '@/hooks/useColorCycle';
+import { MBTIColors } from '@/components/MBTIColors';
 
 
 const tfClassName="w-[80%] mx-auto"
-
+const bgColors=MBTIColors({colorDest:"Primary",mbti:5});
+const secondaryColors=MBTIColors({colorDest:"Secondary",mbti:5});
 
 export default function Login(){
+    //console.log(bgColors);
+    const bgColor=useColorCycle(bgColors,3000);
+    const secColor=useColorCycle(secondaryColors,3000);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [correctFields, setCorrectFields] = useState(true)
+    const [correctFields, setCorrectFields] = useState(true);
+
 
 
     return(
@@ -21,12 +28,22 @@ export default function Login(){
             <Head>
                 <title>M8TI - Sign In</title>
             </Head>
-            <AuthLayout className="pb-[5%]">
-                <h1 className='text-center text-3xl font-bold pt-[3%] text-[#785D87]'>Sign In</h1>
+            <AuthLayout className="pb-[5%]" style={{
+                borderColor: bgColor,
+                background: secColor,
+                transition: "border-color 1s ease-in-out, background-color 1s ease-in-out"
+            }}>
+                <h1 className='text-center text-3xl font-bold pt-[3%]' style={{
+                    color:bgColor,
+                    transition: "color 1s ease-in-out" 
+                    }}>Sign In</h1>
                 <form className='mt-[10%]'>
                     <div>
                       <TextField
                         className={clsx('pb-[10%]', tfClassName)}
+                        labelColor={bgColor}
+                        fieldBGColor={secColor}
+                        isDynamic={true}
                         label="Email or Profile Name"
                         id="email"
                         name="email"
@@ -38,6 +55,10 @@ export default function Login(){
                         <TextField
                         className={tfClassName}
                         label="Password"
+                        labelColor={bgColor}
+                        fieldBGColor={secColor}
+                        isDynamic={true}
+                        inputClassName={`border-[#785D87]`}
                         id="password"
                         name="password"
                         type="password"
@@ -47,7 +68,7 @@ export default function Login(){
                         required/>
                     </div>
                     <div className='pt-[10%] flex justify-between w-[80%] mx-auto'>
-                        <Button color='purple' className='w-[30%]'>Sign in</Button>
+                        <Button color='purple' className='w-[30%] bg-[#FFFFFF]'>Sign in</Button>
                         <Button color='purple' className='w-[30%]'>Sign up</Button>
                     </div>
                 </form>
