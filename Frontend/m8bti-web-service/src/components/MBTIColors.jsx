fba23039557daaa6cfb9c5e9b3937e9f1cb81a17
Torba2207 <alexnychyporchuk@gmail.com>
2025-03-30 05:34:28 +0200

    upd: MBTIColors can return arrays with colors, not only single colors

--- Frontend/m8bti-web-service/src/components/MBTIColors.jsx
+++ Frontend/m8bti-web-service/src/components/MBTIColors.jsx
@@ -1,13 +1,19 @@
 export function MBTIColors({colorDest, mbti}){
-    const colorsPrimary=["#785D87","#33A474","#4298B4","E4AE3A"]
+    const colorsPrimary=["#785D87","#33A474","#4298B4","#E4AE3A"]
     const colorsSecondary=["#E7DFEA", "#D6ECE3","#D9EAF0","#F9EED7"]
+    
     if(colorDest=="Primary"){
-        console.log("Color",colorsPrimary[mbti])
+        //console.log("Color",colorsPrimary[mbti])
+        //console.log("I'm in");
+        if(mbti===5)
+            return colorsPrimary
         return colorsPrimary[mbti]
     }
-    else{
-        console.log("Index", mbti)
-        console.log("Color",colorsSecondary[mbti])
+    else if(colorDest=="Secondary"){
+        //console.log("Index", mbti)
+        //console.log("Color",colorsSecondary[mbti])
+        if(mbti===5)
+            return colorsSecondary
         return colorsSecondary[mbti]
     }
 }
