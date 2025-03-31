57b5ec302c38f5e9dd369e18ef0587fc6f6baf48
Torba2207 <alexnychyporchuk@gmail.com>
2025-03-31 02:37:40 +0200

    feat: whole page background color transition

--- Frontend/m8bti-web-service/src/components/AuthLayout.jsx
+++ Frontend/m8bti-web-service/src/components/AuthLayout.jsx
@@ -4,8 +4,13 @@ import clsx from 'clsx'
 
 export default function AuthLayout({children, ...props}){
     return(
-        <main className='w-screen h-screen min-h-screen'>
-            <div className='w-screen h-[30%] mb-[1%]'>
+        <main className='w-screen h-f min-h-screen'
+            style={{
+                background:props.mainBGColor,
+                transition:'background-color 1s ease-in-out'
+            }}
+        >
+            <div className='w-screen h-[5%] lg:h-[30%] mb-[1%]'>
                 <Link href="/" arial-label="Home">
                     <Logo className="w-auto h-auto mx-auto"/>
                 </Link>
