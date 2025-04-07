66b9132e101de134b00c35543c704046304f692d
Torba2207 <alexnychyporchuk@gmail.com>
2025-04-07 12:12:20 +0200

    feat: Logout functionality

--- Frontend/m8bti-web-service/src/pages/profilePage.jsx
+++ Frontend/m8bti-web-service/src/pages/profilePage.jsx
@@ -6,12 +6,20 @@ import AuthRoute from "@/hooks/route";
 export default function ProfilePage(){
     const {currentUser}=useContext(AuthContext)
     const {userData}=useContext(AuthContext)
+    const {logout}=useContext(AuthContext)
+    
     return(
         <AuthRoute>
             <Head>
                 <title>M8TI - Profile Page</title>
             </Head>
             <h1>{userData!==null?userData.nickname:"Users"}</h1>
+            <button
+      onClick={logout}
+      className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition"
+    >
+      Log Out
+    </button>
         </AuthRoute>
     )
 }
