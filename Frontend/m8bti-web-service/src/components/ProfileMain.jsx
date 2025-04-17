339793b3534482b652a41151ec450137a1384951
Torba2207 <alexnychyporchuk@gmail.com>
2025-04-17 17:35:16 +0200

    fix: refactor+ new width parametr

--- Frontend/m8bti-web-service/src/components/ProfileMain.jsx
+++ Frontend/m8bti-web-service/src/components/ProfileMain.jsx
@@ -3,8 +3,12 @@ import clsx from "clsx";
 export function ProfileMain({primaryColor,secondaryColor,extraColor,mbti,nickname,currentUser, userAbout,
     ...props}){
     return(
-        <div  className={clsx(`w-screen h-f min-h-screen bg-[${secondaryColor}]`)}>
-            <h1 className={clsx(`text-2xl font-bold text-[${primaryColor}]`)}>About {currentUser===nickname?"You":nickname}</h1>
+        <div  className={clsx(`w-f h-f min-h-screen bg-[${secondaryColor}]`)}>
+            <div>
+                <h1 className={clsx(`text-2xl font-bold text-[${primaryColor}]`)}>
+                    About {currentUser===nickname?"You":nickname}
+                </h1>
+            </div>
         </div>
     );
 }
