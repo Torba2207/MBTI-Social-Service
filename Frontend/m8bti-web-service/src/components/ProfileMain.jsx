7d5cd6dd8304ffe85a93e320552f72e5ffadd8f3
Torba2207 <alexnychyporchuk@gmail.com>
2025-04-17 20:24:45 +0200

    upd: Textfield included

--- Frontend/m8bti-web-service/src/components/ProfileMain.jsx
+++ Frontend/m8bti-web-service/src/components/ProfileMain.jsx
@@ -1,13 +1,14 @@
 import clsx from "clsx";
-
+import { TextField } from "./Fields";
 export function ProfileMain({primaryColor,secondaryColor,extraColor,mbti,nickname,currentUser, userAbout,
     ...props}){
     return(
-        <div  className={clsx(`w-f h-f min-h-screen bg-[${secondaryColor}]`)}>
-            <div>
+        <div  className="w-f h-f">
+            <div className="pl-[15%] pt-[5%]">
                 <h1 className={clsx(`text-2xl font-bold text-[${primaryColor}]`)}>
                     About {currentUser===nickname?"You":nickname}
                 </h1>
+                <TextField className={`w-[60%]`}/>
             </div>
         </div>
     );
