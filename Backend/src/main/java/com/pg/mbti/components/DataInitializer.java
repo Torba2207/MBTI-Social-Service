69f6acd28614678ee423d3e4c098149247777d70
Dmytro Dzhusov <dmytro.dzhusov@gmail.com>
2025-04-20 17:12:32 +0200

    feat: Add tag initialization in DataInitializer and create tags.json file

--- Backend/src/main/java/com/pg/mbti/components/DataInitializer.java
+++ Backend/src/main/java/com/pg/mbti/components/DataInitializer.java
@@ -1,10 +1,12 @@
 package com.pg.mbti.components;
 
+import com.pg.mbti.entity.Tag;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pg.mbti.entity.questions.Question;
 import com.pg.mbti.services.QuestionsService;
+import com.pg.mbti.services.TagsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
@@ -20,11 +22,13 @@ import java.util.List;
 @Slf4j
 public class DataInitializer implements CommandLineRunner {
     private final QuestionsService questionsService;
+    private final TagsService tagsService;
     private final ObjectMapper objectMapper;
 
     @Override
     public void run(String... args) {
         initializeQuestions();
+        initializeTags();
     }
 
     private void initializeQuestions() {
@@ -54,6 +58,37 @@ public class DataInitializer implements CommandLineRunner {
             }
         } catch (IOException e) {
             log.error("Error loading questions from JSON file", e);
+        }
+    }
+
+    private void initializeTags() {
+        if (!tagsService.getAllTags().isEmpty()) {
+            log.info("Tags already exist in database. Skipping initialization.");
+            return;
+        }
+
+        try {
+            log.info("Loading tags from JSON file");
+            ClassPathResource resource = new ClassPathResource("data/tags.json");
+            try (InputStream inputStream = resource.getInputStream()) {
+                JsonNode rootNode = objectMapper.readTree(inputStream);
+                List<JsonNode> tagNodes = objectMapper.convertValue(
+                        rootNode.get("tags"),
+                        new TypeReference<>() {}
+                );
+
+                log.info("Found {} tags in JSON file", tagNodes.size());
+                tagNodes.forEach(tagNode -> {
+                    Tag tag = Tag.builder()
+                            .name(tagNode.get("name").asText())
+                            .category(tagNode.get("category").asText())
+                            .build();
+                    tagsService.createTag(tag);
+                });
+                log.info("Successfully initialized tags from JSON file");
+            }
+        } catch (IOException e) {
+            log.error("Error loading tags from JSON file", e);
         }
     }
 }
