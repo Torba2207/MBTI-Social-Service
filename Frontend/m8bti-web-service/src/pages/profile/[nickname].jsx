214e37afe015d9748eefc88ec0cf4bec4b89be15
Torba2207 <alexnychyporchuk@gmail.com>
2025-04-21 18:17:54 +0200

    feat: Fetch Data of visited user

--- Frontend/m8bti-web-service/src/pages/profile/[nickname].jsx
+++ Frontend/m8bti-web-service/src/pages/profile/[nickname].jsx
@@ -1,5 +1,5 @@
 import { AuthContext } from "@/hooks/auth";
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Head from "next/head";
 import AuthRoute from "@/hooks/route";
 import { MBTIColors } from "@/components/MBTIColors";
@@ -9,12 +9,14 @@ import { Button } from "@/components/Button";
 import { Header } from "@/components/Header";
 import { useRouter } from "next/router";
 import { ProfileMain } from "@/components/ProfileMain";
+import autoprefixer from "autoprefixer";
 
 export default function ProfilePage(){
     const router = useRouter();
     const { nickname } = router.query;
     //console.log(nickname)
     const {currentUser}=useContext(AuthContext)
+    const {setLoading}=useContext(AuthContext   )
     if(currentUser===null)
         return(
         <AuthRoute>
@@ -22,13 +24,42 @@ export default function ProfilePage(){
         </AuthRoute>)
     const {userData}=useContext(AuthContext)
     const {logout}=useContext(AuthContext)
-    const groupIndex=userData?.mbtiType
-        ? getMBTIGroupIndex(userData.mbtiType.toString())
-        : 0;
-    console.log(userData)
+    const [userPageData, setUserPageData] =useState(userData)
+
+    const fetchVisitedUserData=async()=>{
+        try{
+            const response = await fetch(`http://localhost:8080/api/users/${nickname}`, {
+                method: "GET",
+                credentials: "include"
+            });
+            if (!response.ok) throw new Error("Failed to fetch user data");
+            const data = await response.json();
+            setUserPageData(data);
+
+        }
+        catch{
+
+        }
+        finally{
+        }
+    }
+    useEffect(()=>{
+        if(nickname!==currentUser){
+            
+            fetchVisitedUserData();
+        }
+    
+    },[])
+
+    const groupIndex=userPageData?.mbtiType
+    ? getMBTIGroupIndex(userPageData.mbtiType.toString())
+    : 0;
+    console.log(userPageData)
     const primaryColor=MBTIColors({colorDest:"Primary",mbti:groupIndex})
     const secondaryColor=MBTIColors({colorDest:"Secondary",mbti:groupIndex})
     const extraColor=MBTIColors({colorDest:"Extra",mbti:groupIndex})
+    console.log(groupIndex);
+    console.log(primaryColor);
     
     return(
         <AuthRoute>
@@ -36,11 +67,13 @@ export default function ProfilePage(){
                 <title>M8TI - {nickname}</title>
             </Head>
             <div className={clsx(`h-f min-h-screen bg-[${secondaryColor}]`)}>
-            <Header mbti={groupIndex} userName={userData.name+" "+userData.surname}/>
+            <Header mbti={groupIndex} userName={userPageData.name+" "+userPageData.surname}/>
             <ProfileMain primaryColor={primaryColor} secondaryColor={secondaryColor}
                 extraColor={extraColor} mbti={groupIndex} nickname={nickname} currentUser={currentUser}
-                userAbout={userData.description}/>
-            <Button color={groupIndex} onClick={logout}> Log Out</Button>
+                userAbout={userPageData.description}/>
+                <div className={currentUser!==nickname?"hidden":""}> 
+                    <Button color={groupIndex} onClick={logout}> Log Out</Button>
+                </div>
             </div>
         </AuthRoute>
     )
