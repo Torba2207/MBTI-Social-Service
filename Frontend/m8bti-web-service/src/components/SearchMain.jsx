be222620b760f715308ade84372b18c0f92d5b04
Oleksandr Nychyporchuk <alexnychyporchuk@gmail.com>
2025-06-01 20:44:18 +0200

    start: search main layout component

---
+++ Frontend/m8bti-web-service/src/components/SearchMain.jsx
@@ -0,0 +1,13 @@
+export default function SearchMain({primaryColor, secondaryColor, extraColor, mbti, ...props}) {
+    return (
+        <div className="w-full h-full min-h-screen"
+            style={{
+                backgroundColor: secondaryColor,
+            }}
+        >
+            
+            
+            
+        </div>
+    );
+}
