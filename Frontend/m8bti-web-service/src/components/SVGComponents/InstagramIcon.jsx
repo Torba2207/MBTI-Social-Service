d2b91489ac3eb54e0969c0a04ca294fdb82599a5
Anton <antonmatveuk@gmail.com>
2025-06-15 23:39:06 +0200

    Added FB and IG SVG Components

---
+++ Frontend/m8bti-web-service/src/components/SVGComponents/InstagramIcon.jsx
@@ -0,0 +1,14 @@
+export function InstagramIcon({ className, fill }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24" // A common viewBox for icons
+      className={className}
+      fill={fill}
+    >
+      <path
+        d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4c0 3.2-2.6 5.8-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8C2 4.6 4.6 2 7.8 2zm-.2 2A4.8 4.8 0 003 7.6v8.8A4.8 4.8 0 007.6 21h8.8A4.8 4.8 0 0021 16.4V7.6A4.8 4.8 0 0016.4 3H7.6zM12 7a5 5 0 100 10 5 5 0 000-10zm0 2a3 3 0 110 6 3 3 0 010-6zM17.3 6a1.2 1.2 0 100 2.4 1.2 1.2 0 000-2.4z"
+      ></path>
+    </svg>
+  );
+}
